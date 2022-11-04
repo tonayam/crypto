1136ea082c23cd4cafb8a104f25f5e26a40f2ae6
tonayam <tonayam99@gmail.com>
2022-11-04 07:24:50 +0100

    About Us page updated

--- src/pages/affiliate/AffiliateProgram.jsx
+++ src/pages/affiliate/AffiliateProgram.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import PageHeader from "../../components/header/PageHeader";
 
 const AffiliateProgram = () => {
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0 });
+    document.title = `Broker - Affiliate Program`;
+  }, []);
   return (
     <section className='affiliate-program'>
       <PageHeader>Affiliate Program</PageHeader>
